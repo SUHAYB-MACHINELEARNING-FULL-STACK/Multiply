4e3a95a9101fbf205dbddce90fe2adf37b5e8dd7
SUHAYB-MACHINELEARNING-FULL-STACK <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-04-25 08:26:11 +0300

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,3 +1,5 @@
 '''Multiply the number 2 over the elements of the matrix '''
-
-mul = lambda list : [ 2 * i for i in list ]
+from random import randint
+mul = lambda list : [ str(randint(1,100)) * i for i in list ]
+for i in range(randint(1,100)):
+  print(f"{'||||||||||'.join(mul([x for x in range(randint(1,100))]))}\n\n\n")
