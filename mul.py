99e803f3928c52d1f332b37ef7e439d7cbea6121
SUHAYB-MACHINELEARNING-FULL-STACK <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-05-07 10:42:31 +0300

    Update mul.py

--- mul.py
+++ mul.py
@@ -2,4 +2,4 @@
 from random import randint
 mul = lambda list : [ randint(1,100) * i for i in list ]
 for i in range(randint(1,100)):
-  print(f"{"         ,        ".join(mul([x for x in range(randint(1,100))]))}\n\n\n")
+  print(f"{"|||||||||||".join(mul([x for x in range(randint(1,100))]))}\n\n\n")
